87d3b160d5a9b47ad46a8eb60630d1ff4d0d771a
Ivan Bashkirov <ivanbashkirov@protonmail.com>
2017-05-04 16:46:27 +0100

    added fake data with faker

--- Gemfile
+++ Gemfile
@@ -20,10 +20,13 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
- gem 'bootstrap-sass'
- gem 'figaro'
- gem 'pundit'
 
+# bootstrap
+ gem 'bootstrap-sass'
+ # secure configuration
+ gem 'figaro'
+ # authorization
+ gem 'pundit'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+include ActionView::Helpers::TextHelper
+
+10.times {User.create!(email: Faker::Internet.unique.email, password: Faker::Internet.password)}
+
+users = User.all
+
+50.times {users.sample.wikis.create!(title: Faker::Lorem.sentence(2), body: Faker::Lorem.paragraph)}
+
+puts "Created #{pluralize(User.all.count, 'user')}"
+puts "Created #{pluralize(Wiki.all.count, 'wiki')}"
